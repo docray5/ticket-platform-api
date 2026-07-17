f849a8f7fd11c449b3574d5e1c93d3d9abebf2c0
Janek <bili1.karol2@gmail.com>
2026-07-17 16:39:04 +0200

    initial commit: setup basic spring, security and docker (with test container) configuration

---
+++ src/test/java/com/bilicki/ticketing/TicketingApplicationTests.java
@@ -0,0 +1,20 @@
+package com.bilicki.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
