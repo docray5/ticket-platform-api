01b1e414e4d68e4ac4fdebea0ce1ae1ad8a43cf8
Janek <bili1.karol2@gmail.com>
2026-07-23 12:32:58 +0200

    add user entity and repository

---
+++ src/main/java/com/bilicki/ticketing/user/internal/User.java
@@ -0,0 +1,41 @@
+package com.bilicki.ticketing.user.internal;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    @Setter
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    @Setter
+    private String passwordHash;
+
+    @Column(
+            name = "role",
+            nullable = false,
+            columnDefinition = "VARCHAR(20) DEFAULT 'CUSTOMER' CHECK (role IN ('ADMIN', 'CUSTOMER'))"
+    )
+    private String role = "CUSTOMER";
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+    public User(String email, String passwordHash) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+}
