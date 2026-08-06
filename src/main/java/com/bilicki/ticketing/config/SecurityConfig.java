b23b7ae8c2316abf760b70e4ad70526cc80ce627
Janek <bili1.karol2@gmail.com>
2026-08-06 18:12:16 +0200

    feat(catalog-public-endpoints): add test for the custom query, add all endpoints to not require auth

--- src/main/java/com/bilicki/ticketing/config/SecurityConfig.java
+++ src/main/java/com/bilicki/ticketing/config/SecurityConfig.java
@@ -44,7 +44,9 @@ public class SecurityConfig {
                                 "/api/v1/auth/register",
                                 "/api/v1/auth/login",
                                 "/api/v1/movies",
-                                "/api/v1/movies/{movieId}"
+                                "/api/v1/movies/{movieId}",
+                                "/api/v1/movies/{movieId}/showtimes",
+                                "/api/v1/showtimes/{showtimeId}/seats"
                         ).permitAll().anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
