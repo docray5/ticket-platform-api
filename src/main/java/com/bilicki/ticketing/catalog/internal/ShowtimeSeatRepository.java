b220368b2aebb859188a3d6077cff332ecfc7733
Janek <bili1.karol2@gmail.com>
2026-08-06 15:41:37 +0200

    fix(catalog-public-endpoints): optimized query for getting all showtime seats, this resulted in reducing the response time for getting the seat map by more than double

--- src/main/java/com/bilicki/ticketing/catalog/internal/ShowtimeSeatRepository.java
+++ src/main/java/com/bilicki/ticketing/catalog/internal/ShowtimeSeatRepository.java
@@ -1,10 +1,14 @@
 package com.bilicki.ticketing.catalog.internal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ShowtimeSeatRepository extends JpaRepository<ShowtimeSeat, UUID> {
+    @Query("""
+        SELECT ss FROM ShowtimeSeat ss JOIN FETCH ss.seat s JOIN FETCH s.seatType WHERE ss.showtime.id = :showtimeId
+    """)
     List<ShowtimeSeat> findAllByShowtimeId(UUID showtimeId);
 }
