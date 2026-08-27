bda197dc1b594a8b97058f45bfe321b699ce1c94
Janek <bili1.karol2@gmail.com>
2026-08-27 14:50:45 +0200

    feat(booking): add not null to uuid's in hold request

--- src/main/java/com/bilicki/ticketing/booking/web/HoldRequest.java
+++ src/main/java/com/bilicki/ticketing/booking/web/HoldRequest.java
@@ -1,12 +1,13 @@
 package com.bilicki.ticketing.booking.web;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.UUID;
 
 public record HoldRequest(
         @NotEmpty(message = "At least one seat must be selected")
-        List<UUID> showtimeSeatIds
+        List<@NotNull UUID> showtimeSeatIds
 ) {
 }
